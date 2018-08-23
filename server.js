bbe6942dd9304355de62e19e6def823160d59a54
mag <mag.it.use@gmail.com>
2018-08-23 22:38:26 +0000

    working on login & register & menu routes, prepare to conbine with database

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
+const app           = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
@@ -35,12 +35,153 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
+
+
+
+/*----------------------Fake Database--------------------*/
+
+const client = [
+  {
+    id: 1,
+    name: "client1",
+    phone: "647-000-0000"
+  },
+  {
+    id: 2,
+    name: "client2",
+    phone: "647-111-111"
+  }
+]
+
+const products = [
+  {
+    id: 1,
+    name: "Coke",
+    description: "Refreshingly cold!",
+    quantity: 0,
+    item_price: 3,
+    categories_id: 3
+  },
+  {
+    id: 2,
+    name: "Big Mister",
+    description: "The signature burger of MrDonald",
+    quantity: 0,
+    item_price: 7,
+    categories_id: 1
+  },
+  {
+    id: 3,
+    name: "Boring Fries",
+    description: "Crispy, fried potato strips",
+    quantity: 0,
+    item_price: 5,
+    categories_id: 2
+  }
+];
+
+const categories = [
+  {
+    id: 1,
+    name: "main"
+  },
+  {
+    id: 2,
+    name: "side"
+  },
+  {
+    id: 3,
+    name: "drink"
+  }
+]
+
+const orderList = [
+  {
+    id: 1,
+    productID: 1,
+    userID: 1
+  },
+  {
+    id: 2,
+    productID: 2,
+    userID: 2
+  }
+];
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+
+/*----------------------My Routes--------------------*/
 
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+
+//Menu page
+app.get("/menu", (req, res) => {
+  let templateVars = {
+    products: products,
+    categories: categories,
+    orderList: orderList
+  };
+  res.render("menu", templateVars);
+});
+
+app.post("/menu", (req, res) => {
+
+});
+
+//Login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const {user_name, password} = req.body;
+  if (user_name  && password) {
+      res.redirect("/");
+  } else {
+    res.send("sorry, please provide all the infor.");
+  }
+});
+
+
+//Register page
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const {user_name, password, phone} = req.body;
+  if (user_name && password && phone) {
+    console.log(client);
+    client.client1 = {
+      id: "user1",
+      name: user_name,
+      phone: phone
+    };
+    console.log(client);
+    res.redirect("/");
+  } else {
+    res.send("sorry, please provide all the infor.");
+  }
+});
+
+app.post("/add/:productID", (req, res) => {
+  //add item into order db;
+
+  //redirect to "/menu"
+  res.redirect("/menu");
+})
+
+//Order page - client view
+app.get("/myorder", (req, res) => {
+  res.render("myorder");
+});
+//Order page - restaurant view
+app.get("/clientorder", (req, res) => {
+  res.render("clientorder");
 });
 
 app.listen(PORT, () => {
